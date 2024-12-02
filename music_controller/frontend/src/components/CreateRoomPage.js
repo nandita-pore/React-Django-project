7375d519f486fc00d6a477ad3d6b8502ca1b68ba
Nandita Pore <nandita@k3ytech.com>
2024-12-02 21:20:19 +0530

    Initial Project SetUp

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CreateRoomPage=()=>{
+    return(
+        <div>
+            <h1>
+                Create Room
+            </h1>
+        </div>
+    )
+}
